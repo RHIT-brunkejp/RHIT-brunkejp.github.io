39c1739986e26486c3e1027ea2d81eac44bb3177
brunkejp <brunkejp@rose-hulman.edu>
2026-09-23 10:52:45 -0400

    Removed unnecessary things. Polishing.

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-let buttons = document.querySelectorAll(".filter");
+/*
+JavaScript references:
+https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+*/
+
 let projects = document.querySelectorAll(".project");
 
-for (let i = 0; i < buttons.length; i++) {
-
-    buttons[i].addEventListener("click", function() {
-
-        let type = buttons[i].getAttribute("data-type");
-
-        for (let j = 0; j < buttons.length; j++) {
-            buttons[j].classList.remove("current-filter");
-        }
-
-        buttons[i].classList.add("current-filter");
-
-        for (let j = 0; j < projects.length; j++) {
-
-            let projectType =
-                projects[j].getAttribute("data-type");
-
-            if (type == "all" || type == projectType) {
-                projects[j].style.display = "block";
-            }
-            else {
-                projects[j].style.display = "none";
-            }
-        }
+for (let i = 0; i < projects.length; i++) {
+    projects[i].addEventListener("click", function() {
+        projects[i].classList.toggle("selected");
     });
 }
