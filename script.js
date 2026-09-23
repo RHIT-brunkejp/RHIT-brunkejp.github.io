a99a5b2b7cffbaad12769b50eca649f2dbabb8b4
brunkejp <brunkejp@rose-hulman.edu>
2026-09-23 11:26:01 -0400

    added auto footer and header via JS

--- script.js
+++ script.js
@@ -2,7 +2,25 @@
 JavaScript references:
 https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
 https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+
+The navigation, footer, and project interaction were independently
+written for this website.
 */
+
+document.getElementById("top").innerHTML = `
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="resume.html">Resume</a>
+        <a href="portfolio.html">Portfolio</a>
+    </nav>
+`;
+
+document.getElementById("bottom").innerHTML = `
+    <p>
+        <a href="https://github.com/RHIT-brunkejp">GitHub</a>
+        <a href="https://www.linkedin.com/in/jjbrunke">LinkedIn</a>
+    </p>
+`;
 
 let projects = document.querySelectorAll(".project");
 
