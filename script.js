90de76d9f7e1e66e5bef15fc8ee16b3c3c66f3a5
brunkejp <brunkejp@rose-hulman.edu>
2026-09-23 10:39:47 -0400

    Polished up the pages (hopefully)

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Portfolio website loaded.");
+
+    let buttons = document.querySelectorAll(".filter-button");
+    let projects = document.querySelectorAll(".project-card");
+
+    for (let i = 0; i < buttons.length; i++) {
+
+        buttons[i].addEventListener("click", function() {
+
+            let selectedCategory = buttons[i].getAttribute("data-filter");
+
+            for (let j = 0; j < buttons.length; j++) {
+                buttons[j].classList.remove("active-filter");
+            }
+
+            buttons[i].classList.add("active-filter");
+
+            for (let j = 0; j < projects.length; j++) {
+
+                let projectCategory =
+                    projects[j].getAttribute("data-category");
+
+                if (selectedCategory == "all" ||
+                    selectedCategory == projectCategory) {
+
+                    projects[j].style.display = "block";
+
+                } else {
+
+                    projects[j].style.display = "none";
+
+                }
+            }
+        });
+    }
 });
